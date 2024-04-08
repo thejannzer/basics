b54330ea2a168817ec39d9bcdf55232ea5a538d5
jasc557d <jannik.scholz@mailbox.tu-dresden.de>
2024-04-08 22:13:25 +0200

    for Loop behandelt

---
+++ Einstieg_Java/for-loop/foor.java
@@ -0,0 +1,11 @@
+//for-loop
+ public class foor{
+    public static void main(String[]args){
+
+        for (int x = 1; x < 10; x++){         //(Variable, Bedingung, Aktualisierung)
+            System.out.println(x);
+        }
+    }
+
+
+ }
