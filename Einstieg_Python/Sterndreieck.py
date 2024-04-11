99faec9593bb7bb7670a87918c03028358919651
jasc557d <jannik.scholz@mailbox.tu-dresden.de>
2024-04-11 21:40:36 +0200

    Sterndreieck in Python

---
+++ Einstieg_Python/Sterndreieck.py
@@ -0,0 +1,11 @@
+#Steuerrechner
+x = "*"
+counter_x = 0
+counter = 0
+
+while counter <= 10:
+    print(counter_x * x)
+
+    if (counter_x == counter):
+        counter_x = counter_x +1
+        counter = counter +1
