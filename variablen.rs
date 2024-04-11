963b3c0cccb0935a90fd0d6303ae6ce1daad3a4a
jasc557d <jannik.scholz@mailbox.tu-dresden.de>
2024-04-11 21:22:06 +0200

    Rust Variablen

---
+++ variablen.rs
@@ -0,0 +1,8 @@
+fn main() {
+    //Variablen
+    let x = 4;     //legt Variable x fest, i32 ist Datentyp (int 32 bit) kann auch verändert werden
+    println!("x is: {}", x);    //mut muted die Variable x
+    let y = "hello";
+    println!("{}", y);
+    
+}
