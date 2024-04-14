0ba3ad25311a445bfda09246fd4d981d9d3978c8
thejannzer <scholz.carlsfeld@gmail.com>
2024-04-14 19:54:24 +0200

    OOP üben

---
+++ Einstieg_Java/Arrays/arrays.java
@@ -0,0 +1,12 @@
+//Arrays
+
+public class arrays{
+    public static void main(String[]args){
+        String[] cars = {"Camaro", "Corvette", "Audi"};    //[] nach Datentyp
+
+        cars[0] = "BMW";     //ersetzt das erste Element der Liste mit BMW
+
+        System.out.println(cars[2]);
+
+    }
+}
