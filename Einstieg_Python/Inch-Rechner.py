6fa580c6cc3f949924545dcf70464afd74994c18
thejannzer <scholz.carlsfeld@gmail.com>
2024-04-14 14:05:20 +0200

    Projekt machen

--- Einstieg_Python/Inch-Rechner.py
+++ Einstieg_Python/Inch-Rechner.py
@@ -1,17 +1,22 @@
-print("Inch-Rechner")
-zahl = input("Bitte gib den Wert in cm ein: ")
-try:
-    zahl = int (zahl)
-except:
-    print("Bitte gib eine ganze Zahl ein")
-    
-if type(zahl)==str:
-    print("Nur Datentyp Integer oder Float sind erlaubt")
-else:
-    while zahl != 0:
-        print (f"Der wert in Inch ist {zahl/2.54}")
-        break
+print ("Inch Rechner")
 
+operation: str = input ("wollen Sie in Inch oder cm umrechen (inch/cm): ")
+if operation == "inch":
+    try:
+        wert = input ("Bitte geben Sie den Wert in cm ein: ")
+        wert = float (wert)
+    except:
+        print("Bitte gib eine Zahl ein")
 
+    print(f"{wert}cm entsprechen {wert/2.54} Inch")
+
+if operation == "cm":
+    try:
+        wert = input ("Bitte geben Sie den Wert in Inch ein: ")
+        wert = float (wert)
+    except:
+        print ("Bitte gib eine Zahl ein")
+
+    print (f"{wert} Inch entsprechen {wert*2.54}cm")
 
     
