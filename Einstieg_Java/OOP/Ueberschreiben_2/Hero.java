12f35c65509f6476b9c068bb63c1134c7ae52a6a
thejannzer <scholz.carlsfeld@gmail.com>
2024-05-08 12:15:25 +0200

    Super Keyword wiederholt

--- Einstieg_Java/OOP/Ueberschreiben_2/Hero.java
+++ Einstieg_Java/OOP/Ueberschreiben_2/Hero.java
@@ -1,9 +1,10 @@
+//"super" wird verwendet um auf Elemente der übergeordneten Klassen zuzugreifen
 public class Hero extends Person {
 
     String power;
 
     public Hero(String name, int age, String power){
-        super(name,age);        //Überschreibt name und alter der Person
+        super(name,age);        //greift auf name und alter der Person zu
         this.power = power;     //fügt power hinzu
     }
     
--- Einstieg_Java/OOP/Vererben_Ueberschreiben/bmw.java
+++ Einstieg_Java/OOP/Vererben_Ueberschreiben/bmw.java
@@ -1,5 +1,6 @@
 public class bmw extends auto {
     //bmw soll 5 km/h schneller sein als ein normales Auto
+    //"super" wird verwendet um auf Elemente der übergeordneten Klassen zuzugreifen (nicht überschreiben)
 
     @Override
     public int getGeschwindigkeit(int Geschwindigkeit){
