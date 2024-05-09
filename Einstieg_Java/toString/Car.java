8660bfcdf50fd82fef5495753a8dc14290cbba9d
thejannzer <scholz.carlsfeld@gmail.com>
2024-05-09 16:43:56 +0200

    toString()

---
+++ Einstieg_Java/toString/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+
+    String make = "Ford";
+    String model = "Mustang";
+    String color = "red";
+    int year = 2021;
+    
+    public String toString(){
+        String myString = make + "\n" + model + "\n" + color + "\n" + year;
+        return myString;
+    }
+}
---
+++ Einstieg_Java/toString/Main.java
@@ -0,0 +1,13 @@
+//toString() liefert String Repräsentation des Objekts
+public class Main{
+    public static void main(String[] args) {
+        
+        Car car = new Car();
+        
+        System.out.println(car);
+
+        //ohne toString() würde Adresse des car Objekts geprintet werden
+
+
+    }
+}
