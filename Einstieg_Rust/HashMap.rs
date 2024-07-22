632b80387254a7ad762f9464c01edfa3085014ab
thejannzer <scholz.carlsfeld@gmail.com>
2024-07-22 15:07:07 +0200

    Klausurvorbereitung

--- Einstieg_Rust/HashMap.rs
+++ Einstieg_Rust/HashMap.rs
@@ -3,7 +3,7 @@
 use std::collections::HashMap;
 fn main() {
     let mut scores = HashMap::new();
-    scores.insert(String::from("Alice"), 1.3);
+    scores.insert(String::from("Alice"), 1.3);          //insert iseriert Wertepaar in Map
     scores.insert(String::from("Bob"), 2.0);
 
     //mit get() werden Daten erreicht
@@ -13,4 +13,20 @@ fn main() {
     for (key, value) in &scores {
         println!("{key}: {value:.1}");
     }
+
+
+    //entry() wird verwendet um auf einen Schlüssel zuzugreifen
+    let entry = scores.entry(String::from("Bob"));      //mögliche Auswertung mit match
+
+
+    //or_insert() wird auf entry angewendet und fügt einen Schlüssel hinzu, falls keiner existiert
+    let value = scores.entry(String::from("Konrad")).or_insert(3.0);
+    println!("Der Wert von Konrad ist {}", value);
+
+
+    //and_modify() wird auf entry angewendet und erlaubt es den Wert eines existierenden 
+    //Schlüssels zu ändern. Wenn der Key nicht existiert, passiert nix
+    scores.entry(String::from("Alice")).and_modify(|e| {*e += 1.0});    //Closure e verändert der Wert
+    println!("Der modifizierte Wert von Alice ist: {}", scores["Alice"]);
+    
 }
--- Einstieg_Rust/Iterators.rs
+++ Einstieg_Rust/Iterators.rs
@@ -1,18 +1,30 @@
 //Iterators sind Teil der funktionalen Programmierung in Rust
-//vereinfacht looping
+//vereinfacht looping... alternativ mit for oder while Schleifen
 fn main() {
     let numbers = vec![1, 2, 3, 4, 5];
 
     for num in numbers.iter(){       //schneller als ohne .iter()
     println!("{}", num);
    }
-
+    let v0 = vec![2, 4, 8, 16];
+    let v = vec![1, 4, 8, 11, 15, 20];
     let v1 = vec![1, 2, 3, 4, 5];
-    let v2 = v1.into_iter()            //Iterator
-        .map(|item| item + 1)   //Closure
-        .collect::<Vec<_>>();                                 //Sammelt alle Elemente wieder in einem Vector
+
+    let v2 = v1.into_iter()                         //Iterator
+                    .map(|item| item + 1)    //Closure... Map wendet Funktion auf jedes Element an
+                    .collect::<Vec<_>>();                                  //Sammelt alle Elemente wieder in einem Vector
+
+    let v3 = v.into_iter()
+                    .filter(|&x| x%2 == 0)
+                    .collect::<Vec<_>>();                  //filter filtert Elemente nach Bedingung
+
+    let v4 = v0.into_iter()
+                    .find(|&x| x > 7);            //find gibt das erste Element zurück, dass Bedingung erfüllt
+
 
     println!("{:?}", v2);
+    println!("{:?}", v3);
+    println!("{:?}", v4);
 
     //Iterators verhalten sich "lazy"... heißt sie müssen vom User beendet werden
     //das geschieht mit der collect() Methode
